437de0333881e467a17266156cb3e9b818d85550
DrSkyFox <52832487+DrSkyFox@users.noreply.github.com>
2021-12-07 22:12:49 +0300

    schedul

---
+++ castilizer-scheduller/src/main/java/oba/castilizer/CastilizerSchedullerApplication.java
@@ -0,0 +1,13 @@
+package oba.castilizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastilizerSchedullerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CastilizerSchedullerApplication.class, args);
+    }
+
+}
---
+++ castilizer-scheduller/src/test/java/oba/castilizer/CastilizerSchedullerApplicationTests.java
@@ -0,0 +1,13 @@
+package oba.castilizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastilizerSchedullerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
